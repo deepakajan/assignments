c1cd1641c725ec6a0c3d3d630f9ab1e6cff72fa6
deepakajan <96662741+deepakajan@users.noreply.github.com>
2022-01-05 22:46:31 +0530

    Add files via upload

---
+++ day2/LearnnWhileLoop.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class LearnnWhileLoop {
+	
+	public static void main(String[] args) {
+		int x=5;
+		while (x < 10) {
+			System.out.println(x);
+			x = x+1;
+		}
+	}
+}
