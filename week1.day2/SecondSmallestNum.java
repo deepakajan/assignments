e0637a5f2d1766b57b97ea2ea159eea4911da679
deepakajan <96662741+deepakajan@users.noreply.github.com>
2022-01-07 20:16:50 +0530

    Add files via upload

---
+++ week1.day2/SecondSmallestNum.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+	import java.util.Arrays;
+
+	public class SecondSmallestNum {
+
+		public static void main(String[] args) {
+			int[] number= {1,9,6,7,5,10};
+			Arrays.sort(number);
+			System.out.println("Second smallest number is:"+number[1]);
+		}
+
+	}
+
